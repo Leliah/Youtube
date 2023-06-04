984b1faa9ceafe6e303b46a076f5548e00ed9649
LELIAH <LELIAH791@GMAIL.COM>
2023-06-04 16:08:03 -0400

    updated code

--- youtube-app/src/components/Home.js
+++ youtube-app/src/components/Home.js
@@ -1,39 +1,40 @@
 import React from 'react'
 import './Home.css'
 import { useEffect, useState } from 'react';
+import { getAllVideos } from '../api/fetch';
 
 function Home() {
-    const [mostPopularList, setMostPopularList] = useState([]);
+
+    const [allVideos, setAllVideos] = useState([]);
 
     useEffect(() => {
-        fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=20&key=${process.env.REACT_APP_API_KEY}`)
-        .then(res => res.json())
-        .then(response => {
-            //console.log(response, 'tube of you');
+        getAllVideos()
+          .then((response) => {
+            setAllVideos(response.items);
+            //  console.log(allVideos)
+          })
+           .catch((error) => {
+             console.error(error);
+           });
+      }, []);
 
-            let popularVideo = response.items;
-            console.log(popularVideo)
-            
-            let trendingVideo = popularVideo.map((video, index) => {
-                console.log(video);
 
-                let popularVImg = video.snippet.thumbnails.medium.url;
-                return(
-                    <li key={index}>
-                        <img src={popularVImg} alt='pop-vid-thumbnail'/>
-                        <h2>{video.snippet.title}</h2>
-                    </li>
-                )
-            })
-            setMostPopularList(trendingVideo);
-        });
-
-    })
 
   return (
     <div>
         <div className='most-popular-results'>
-            {mostPopularList}
+            {
+                allVideos.map((video, index) => {
+                    // console.log(video)
+                    let popularVImg = video.snippet.thumbnails.medium.url;
+                    return(
+                      <li key={index}>
+                          <img src={popularVImg} alt='pop-vid-thumbnail'/>
+                          <h2>{video.snippet.title}</h2>
+                      </li>
+                    )
+                })
+            }
         </div>
     </div>
   )
