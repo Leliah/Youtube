c906b0c5dd73912fc9159a96e4938ddcc6bc912a
LELIAH <LELIAH791@GMAIL.COM>
2023-06-07 16:26:53 -0400

    left out semi colon

--- youtube-app/src/App.js
+++ youtube-app/src/App.js
@@ -1,5 +1,5 @@
 import Home from './components/Home';
-import Header from './components/Header'
+import Header from './components/Header';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import VideoPage from './components/VideoListing';
